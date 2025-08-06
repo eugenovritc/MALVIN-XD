8e788226f8f4657766f838366e2910e66835cef0
eugenovritc <eugeninhio@outlook.com>
2025-08-06 20:40:17 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~g7EH2JCB#8lhgs2NOaWMeGPgn2Thqbt8IFQaVvTwXWMIx4M-kBwQ",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "22891235102", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "Africa/Togo" //put your country timeZone....leave blank if u don't know.
 };
